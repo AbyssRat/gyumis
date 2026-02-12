12d5178ef5252f81562f894294a9d3a79f0ab375
AbyssRat <yunothefurry@gmail.com>
2026-02-12 13:14:36 +0100

    ok

--- gyumolcs-frontend/src/api/fruits.js
+++ gyumolcs-frontend/src/api/fruits.js
@@ -1,29 +1,30 @@
-const API_URL = "http://localhost:5000/gyumolcs";
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/gyumolcs"; // backend route
+
+// Alapértelmezett axios config (minden hívásnál használja)
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true, // kell, ha cookie-t vagy auth fejléceket használsz
+  headers: { "Content-Type": "application/json" },
+});
 
 export const getFruits = async () => {
-  const res = await fetch(API_URL);
-  return res.json();
+  const res = await api.get("/");
+  return res.data;
 };
 
 export const createFruit = async (data) => {
-  const res = await fetch(API_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
-  return res.json();
+  const res = await api.post("/", data);
+  return res.data;
 };
 
 export const updateFruit = async (id, data) => {
-  const res = await fetch(`${API_URL}/${id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
-  return res.json();
+  const res = await api.put(`/${id}`, data);
+  return res.data;
 };
 
 export const deleteFruit = async (id) => {
-  const res = await fetch(`${API_URL}/${id}`, { method: "DELETE" });
-  return res.json();
+  const res = await api.delete(`/${id}`);
+  return res.data;
 };
