e8ff7a098b95b09e0b94c19e674712d2e6b8e296
AbyssRat <yunothefurry@gmail.com>
2026-02-13 09:06:33 +0100

    classok

---
+++ gyumolcs-gui/GUI_gyumolcsos/GUI_gyumolcsos/gyumolcs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_gyumolcsos
+{
+    internal class gyumolcs
+    {
+        public int gyumolcsid;
+        public string nev;
+        public string megjegyzes;
+        public string nev_eng;
+        public string alt_szoveg;
+        public string src;
+
+    }
+}
